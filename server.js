32de91ca4c0bff464f7d1f704d8e91234d3d9cfb
Elena <freshjs.ftm@gmail.com>
2022-02-04 12:08:29 +0200

    Add validation

--- server.js
+++ server.js
@@ -8,13 +8,34 @@ mongoose
 .connect('mongodb://localhost:27017/fm_mongoose')
 .catch(error => console.log(error));
 
+
+
 const taskSchema = new Schema({
-  description:String,
-  isDone:Boolean,
+  description:{ 
+    type: String,
+    required:[true, 'must be'],
+    validate:{
+      validator: (v)=>/[A-Z][a-z\s!]{5,200}/.test(v),
+      message: '{VALUE} must be letter!'
+    },
+  },
+  isDone:{ type: Boolean, default:false },
   dateAt:{ type: Date, default: Date.now },
   author:{
-    name:String,
-    age:Number
+    name:{ 
+      type: String,
+      required:true
+    },
+    email:{ 
+      type: String,
+      required:true
+    },
+    age:{
+      type: Number,
+      validate:{
+        validator: (v)=>v>0
+      }
+    }
   }
 })
 const Task = mongoose.model('Task', taskSchema);
