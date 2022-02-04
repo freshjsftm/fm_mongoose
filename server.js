9889a90d3a3278c0e469c2db78efe088cb8aa690
Elena <freshjs.ftm@gmail.com>
2022-02-04 11:35:21 +0200

    Add POST, GET

--- server.js
+++ server.js
@@ -1,9 +1,44 @@
 const http = require('http');
 const express = require('express');
 const yup = require('yup');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+mongoose
+.connect('mongodb://localhost:27017/fm_mongoose')
+.catch(error => console.log(error));
+
+const taskSchema = new Schema({
+  description:String,
+  isDone:Boolean,
+  dateAt:{ type: Date, default: Date.now },
+  author:{
+    name:String,
+    age:Number
+  }
+})
+const Task = mongoose.model('Task', taskSchema);
+
 
 const app = express();
 app.use(express.json());
+app.post('/', async (req, res, next)=>{
+  try {
+    const {body} = req;
+    const newTask = await Task.create(body);
+    res.send(newTask)
+  } catch (error) {
+    next(error)
+  }
+});
+app.get('/', async (req, res, next)=>{
+  try {
+    const tasks = await Task.find();
+    res.send(tasks)
+  } catch (error) {
+    
+  }
+});
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
